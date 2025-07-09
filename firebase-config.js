70a7ee5104427815cc5e890f3ecd27282a209b5a
ONTech-cloud <omran.najdi29@gmail.com>
2025-07-08 22:20:01 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,17 @@
 // firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD...TonCodeIci...",
-  authDomain: "agence-voyage.firebaseapp.com",
-  projectId: "agence-voyage",
-  storageBucket: "agence-voyage.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+  apiKey: "AIzaSyB9UUdqL1tnF5zHBbFzfNszStvOe8DRDJE",
+  authDomain: "agence-voyage-2c207.firebaseapp.com",
+  projectId: "agence-voyage-2c207",
+  storageBucket: "agence-voyage-2c207.appspot.com",
+  messagingSenderId: "166889748731",
+  appId: "1:166889748731:web:24e0ba99ecc4d9b0acd85e"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db };
+export { app, db };
